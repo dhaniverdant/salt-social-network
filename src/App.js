95f0b4006ec060fdedf9eeb940f5abd80a4e2ed5
dhani <dhaniverdant23@gmail.com>
2021-12-25 23:50:44 +0800

    modify app and index styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
+import title from '../src/assets/json/main_box.json';
+import Header from './components/Header';
+import MenuTiles from './components/MenuTiles';
 
 function App() {
+  const renderBoxMainTitle = title.map((data, index) => {
+    return (
+      <div key={index}>
+        <MenuTiles
+          sectionTitle={data.main_title}
+          showAll={data.show_all}
+          inBoxTitle={data.in_box_title}
+          inBoxSubtitle={data.in_box_subtitle}
+          inBoxRightSubtitle={data.in_box_right_subtitle}
+        />
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="content-wrapper">
+        <Header />
+        {renderBoxMainTitle}
+      </div>
     </div>
   );
 }
